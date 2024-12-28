7e7c844fe4de78f32773a075f67585cdbbbcd1cf
mozzie <joshuamutiso455@gmail.com>
2024-12-28 14:02:16 +0300

    Create an UPDATE endpoint

--- server.py
+++ server.py
@@ -46,6 +46,19 @@ def create_contacts():
 
     return new_contact
 
+@app.put('contacts/<id>')
+def update_contact(id):
+    for contact in contacts:
+        if contact['id'] == id:
+            contact['name'] = request.json['name'] if 'name' in request.json else contact['name']
+            contact['phone'] = request.json['phone'] if 'phone' in request.json else contact['phone']
+            return contact
+    return f'There is no contact with that id'
+     
+# GET /contacts - list
+# GET /contacts/<id> - read
+# POST /contacts - create
+# PUT /contacts/<id> - update
 
 if __name__ == '__main__':
     app.run(debug=True)    
